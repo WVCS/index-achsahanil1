8a948e20663c3794f4cc2d4a8d3540f1baff0038
achsahanil1 <achsahanil1@gmail.com>
2021-05-24 18:55:58 -0700

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  
+}
+
+alert("Try Scrolling, Nav Bar is to your top left");
